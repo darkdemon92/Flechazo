6747a7d78fdeda160ea31b723ad0407c8b083be6
Jorge Luis Durán Montero <50774047+darkdemon92@users.noreply.github.com>
2023-06-14 16:07:09 -0400

    PWA
    PWA Modify

--- vite.config.js
+++ vite.config.js
@@ -11,25 +11,6 @@ export default defineConfig({
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
-      includeAssets: ['icon.png'],
-      manifest: {
-        name: 'CubAmor',
-        short_name: 'CubAmor',
-        description: 'Citas para Cubanos/@s',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
     }),
   ],
 });
